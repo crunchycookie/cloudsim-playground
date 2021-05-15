13fe3eca80362e4da61997430a50c75f64f0938f
Tharindu Bandara <tharindu.bandara.edu@gmail.com>
2021-05-15 12:41:40 +0530

    Reformat with google code style guide

--- src/main/java/org/crunchycookie/playground/cloudsim/builders/DatacenterCharacteristicsBuilder.java
+++ src/main/java/org/crunchycookie/playground/cloudsim/builders/DatacenterCharacteristicsBuilder.java
@@ -8,83 +8,91 @@
 
 package org.crunchycookie.playground.cloudsim.builders;
 
+import java.util.List;
 import org.cloudbus.cloudsim.DatacenterCharacteristics;
 import org.cloudbus.cloudsim.Host;
-
-import java.util.List;
 
 /**
  * A builder class to build datacenter characteristics in much friendly manner.
  */
 public class DatacenterCharacteristicsBuilder {
 
-    String systemArchitecture;
-    String operatingSystem;
-    String vmm;
-    double timeZoneOfTheLocation;
-    double costPerUsingProcessing;
-    double costPerUsingMemory;
-    double costPerUsingStorage;
-    double costPerUsingBandwidth;
-    List<Host> hostsList;
+  String systemArchitecture;
 
-    public DatacenterCharacteristicsBuilder withSystemArchitecture(String systemArchitecture) {
-        this.systemArchitecture = systemArchitecture;
-        return this;
-    }
+  String operatingSystem;
 
-    public DatacenterCharacteristicsBuilder withOperatingSystem(String operatingSystem) {
-        this.operatingSystem = operatingSystem;
-        return this;
-    }
+  String vmm;
 
-    public DatacenterCharacteristicsBuilder withVmm(String vmm) {
-        this.vmm = vmm;
-        return this;
-    }
+  double timeZoneOfTheLocation;
 
-    public DatacenterCharacteristicsBuilder withTimeZoneOfTheLocation(Double timeZoneOfTheLocation) {
-        this.timeZoneOfTheLocation = timeZoneOfTheLocation;
-        return this;
-    }
+  double costPerUsingProcessing;
 
-    public DatacenterCharacteristicsBuilder withCostPerUsingProcessing(Double costPerUsingProcessing) {
-        this.costPerUsingProcessing = costPerUsingProcessing;
-        return this;
-    }
+  double costPerUsingMemory;
 
-    public DatacenterCharacteristicsBuilder withCostPerUsingMemory(Double costPerUsingMemory) {
-        this.costPerUsingMemory = costPerUsingMemory;
-        return this;
-    }
+  double costPerUsingStorage;
 
-    public DatacenterCharacteristicsBuilder withCostPerUsingStorage(Double costPerUsingStorage) {
-        this.costPerUsingStorage = costPerUsingStorage;
-        return this;
-    }
+  double costPerUsingBandwidth;
 
-    public DatacenterCharacteristicsBuilder withCostPerUsingBandwidth(Double costPerUsingBandwidth) {
-        this.costPerUsingBandwidth = costPerUsingBandwidth;
-        return this;
-    }
+  List<Host> hostsList;
 
-    public DatacenterCharacteristicsBuilder withHosts(List<Host> hostsList) {
-        this.hostsList = hostsList;
-        return this;
-    }
+  public DatacenterCharacteristicsBuilder withSystemArchitecture(String systemArchitecture) {
+    this.systemArchitecture = systemArchitecture;
+    return this;
+  }
 
-    public DatacenterCharacteristics build() {
+  public DatacenterCharacteristicsBuilder withOperatingSystem(String operatingSystem) {
+    this.operatingSystem = operatingSystem;
+    return this;
+  }
 
-        return new DatacenterCharacteristics(
-                this.systemArchitecture,
-                this.operatingSystem,
-                this.vmm,
-                this.hostsList,
-                this.timeZoneOfTheLocation,
-                this.costPerUsingProcessing,
-                this.costPerUsingMemory,
-                this.costPerUsingStorage,
-                this.costPerUsingBandwidth
-        );
-    }
+  public DatacenterCharacteristicsBuilder withVmm(String vmm) {
+    this.vmm = vmm;
+    return this;
+  }
+
+  public DatacenterCharacteristicsBuilder withTimeZoneOfTheLocation(Double timeZoneOfTheLocation) {
+    this.timeZoneOfTheLocation = timeZoneOfTheLocation;
+    return this;
+  }
+
+  public DatacenterCharacteristicsBuilder withCostPerUsingProcessing(
+      Double costPerUsingProcessing) {
+    this.costPerUsingProcessing = costPerUsingProcessing;
+    return this;
+  }
+
+  public DatacenterCharacteristicsBuilder withCostPerUsingMemory(Double costPerUsingMemory) {
+    this.costPerUsingMemory = costPerUsingMemory;
+    return this;
+  }
+
+  public DatacenterCharacteristicsBuilder withCostPerUsingStorage(Double costPerUsingStorage) {
+    this.costPerUsingStorage = costPerUsingStorage;
+    return this;
+  }
+
+  public DatacenterCharacteristicsBuilder withCostPerUsingBandwidth(Double costPerUsingBandwidth) {
+    this.costPerUsingBandwidth = costPerUsingBandwidth;
+    return this;
+  }
+
+  public DatacenterCharacteristicsBuilder withHosts(List<Host> hostsList) {
+    this.hostsList = hostsList;
+    return this;
+  }
+
+  public DatacenterCharacteristics build() {
+
+    return new DatacenterCharacteristics(
+        this.systemArchitecture,
+        this.operatingSystem,
+        this.vmm,
+        this.hostsList,
+        this.timeZoneOfTheLocation,
+        this.costPerUsingProcessing,
+        this.costPerUsingMemory,
+        this.costPerUsingStorage,
+        this.costPerUsingBandwidth
+    );
+  }
 }
--- src/main/java/org/crunchycookie/playground/cloudsim/builders/HostBuilder.java
+++ src/main/java/org/crunchycookie/playground/cloudsim/builders/HostBuilder.java
@@ -8,6 +8,8 @@
 
 package org.crunchycookie.playground.cloudsim.builders;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.cloudbus.cloudsim.Host;
 import org.cloudbus.cloudsim.Pe;
 import org.cloudbus.cloudsim.VmSchedulerTimeShared;
@@ -15,65 +17,67 @@ import org.cloudbus.cloudsim.provisioners.BwProvisionerSimple;
 import org.cloudbus.cloudsim.provisioners.PeProvisionerSimple;
 import org.cloudbus.cloudsim.provisioners.RamProvisionerSimple;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * A builder class to build a datacenter host in much friendly manner.
  */
 public class HostBuilder {
 
-    int id;
-    int numberOfCores;
-    int mipsPerCore;
-    int amountOfRamInGBs;
-    int amountOfStorageInTBs;
-    int bandwidth;
+  int id;
 
-    public HostBuilder(int id) {
-        this.id = id;
+  int numberOfCores;
+
+  int mipsPerCore;
+
+  int amountOfRamInGBs;
+
+  int amountOfStorageInTBs;
+
+  int bandwidth;
+
+  public HostBuilder(int id) {
+    this.id = id;
+  }
+
+  public HostBuilder withNumberOfCores(int numberOfCores) {
+    this.numberOfCores = numberOfCores;
+    return this;
+  }
+
+  public HostBuilder withMipsPerCore(int mipsPerCore) {
+    this.mipsPerCore = mipsPerCore;
+    return this;
+  }
+
+  public HostBuilder withAmountOfRamInGBs(int amountOfRamInGBs) {
+    this.amountOfRamInGBs = amountOfRamInGBs;
+    return this;
+  }
+
+  public HostBuilder withBandwidth(int bandwidth) {
+    this.bandwidth = bandwidth;
+    return this;
+  }
+
+  public HostBuilder withAmountOfStorageInGBs(int amountOfStorageInTBs) {
+    this.amountOfStorageInTBs = amountOfStorageInTBs;
+    return this;
+  }
+
+  public Host build() {
+
+    // Create processing elements;
+    List<Pe> processingElements = new ArrayList<>();
+    for (int coreIndex = 0; coreIndex < this.numberOfCores; coreIndex++) {
+      processingElements.add(new Pe(coreIndex, new PeProvisionerSimple(this.mipsPerCore)));
     }
 
-    public HostBuilder withNumberOfCores(int numberOfCores) {
-        this.numberOfCores = numberOfCores;
-        return this;
-    }
-
-    public HostBuilder withMipsPerCore(int mipsPerCore) {
-        this.mipsPerCore = mipsPerCore;
-        return this;
-    }
-
-    public HostBuilder withAmountOfRamInGBs(int amountOfRamInGBs) {
-        this.amountOfRamInGBs = amountOfRamInGBs;
-        return this;
-    }
-
-    public HostBuilder withBandwidth(int bandwidth) {
-        this.bandwidth = bandwidth;
-        return this;
-    }
-
-    public HostBuilder withAmountOfStorageInGBs(int amountOfStorageInTBs) {
-        this.amountOfStorageInTBs = amountOfStorageInTBs;
-        return this;
-    }
-
-    public Host build() {
-
-        // Create processing elements;
-        List<Pe> processingElements = new ArrayList<>();
-        for (int coreIndex = 0; coreIndex < this.numberOfCores; coreIndex++) {
-            processingElements.add(new Pe(coreIndex, new PeProvisionerSimple(this.mipsPerCore)));
-        }
-
-        return new Host(
-                this.id,
-                new RamProvisionerSimple(this.amountOfRamInGBs),
-                new BwProvisionerSimple(this.bandwidth),
-                this.amountOfStorageInTBs,
-                processingElements,
-                new VmSchedulerTimeShared(processingElements)
-        );
-    }
+    return new Host(
+        this.id,
+        new RamProvisionerSimple(this.amountOfRamInGBs),
+        new BwProvisionerSimple(this.bandwidth),
+        this.amountOfStorageInTBs,
+        processingElements,
+        new VmSchedulerTimeShared(processingElements)
+    );
+  }
 }
